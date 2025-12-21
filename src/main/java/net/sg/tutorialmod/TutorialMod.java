0697d10c56ecce8b8d111ecdca25d3258bb86ac2
837100 <no8371@gmail.com>
2025-12-22 03:50:08 +0900

    커스텀 아이템 추가 튜토리얼

---
+++ src/main/java/net/sg/tutorialmod/TutorialMod.java
@@ -0,0 +1,18 @@
+package net.sg.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.sg.tutorialmod.item.Moditems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		Moditems.registerModItems();
+	}
+}
