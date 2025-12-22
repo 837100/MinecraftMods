a5327d0a392a608bca44e92db8da0829c4e0de14
837100 <no8371@gmail.com>
2025-12-22 21:25:06 +0900

    핑크 가넷 블록 및 원석 추가, 블록 등록 기능 구현

--- src/main/java/net/sg/tutorialmod/TutorialMod.java
+++ src/main/java/net/sg/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.sg.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sg.tutorialmod.block.ModBlocks;
 import net.sg.tutorialmod.item.Moditems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		Moditems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
